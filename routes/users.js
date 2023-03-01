0c699664f5d6ee4afaefe8c889280e0cd4cdc2ab
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-03-01 20:36:12 +0100

    :tada: first commit

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+let users = [
+  { id: 1, name: 'John', lastName: 'Doe' },
+  { id: 2, name: 'Jane', lastName: 'Doe' },
+  { id: 3, name: 'Adam', lastName: 'Row' },
+  { id: 4, name: 'Anne', lastName: 'Now' },
+  { id: 5, name: 'Bert', lastName: 'Low' },
+];
+
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.json(users);
+});
+
+module.exports = router;
